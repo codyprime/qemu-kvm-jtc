7879db7e9c09b92d9af1c143fbe2cc212ec89e4b
Marcelo Tosatti <mtosatti@redhat.com>
2011-10-27 10:34:42 -0200

    Revert "qemu-kvm: set default accelerator via target.conf"
    This reverts commit a5c40eb1aa929a03157252db7337bc4ddd2435be.
    
    Jan says:
    
    "Unless you fully install qemu, that conf is not picked up (or an older
    version is used). I'd still like to see this working with qemu started
    from a build directory..."
    
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>

--- vl.c
+++ vl.c
@@ -2127,8 +2127,13 @@ static int configure_accelerator(void)
     }
 
     if (p == NULL) {
+#ifdef CONFIG_KVM_OPTIONS
+        /* Use the default "accelerator", kvm */
+        p = "kvm";
+#else
         /* Use the default "accelerator", tcg */
         p = "tcg";
+#endif
     }
 
     while (!accel_initalised && *p != '\0') {
