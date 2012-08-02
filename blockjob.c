f32ee5a087be02208eb556cb3825eb569d98a99a
Jeff Cody <jcody@redhat.com>
2012-08-02 15:57:52 -0400

    Fix for patch #4 of Paolo's block series - initialize BlockJobInfo data elements to be 0.

--- blockjob.c
+++ blockjob.c
@@ -206,7 +206,7 @@ void block_job_sleep_ns(BlockJob *job, QEMUClock *clock, int64_t ns)
 
 BlockJobInfo *block_job_query(BlockJob *job)
 {
-    BlockJobInfo *info = g_new(BlockJobInfo, 1);
+    BlockJobInfo *info = g_new0(BlockJobInfo, 1);
     info->type      = g_strdup(job->job_type->job_type);
     info->device    = g_strdup(bdrv_get_device_name(job->bs));
     info->len       = job->len;
