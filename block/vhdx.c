9edf3fc143aa0b740d56111cf21aea57016af83d
Jeff Cody <jcody@redhat.com>
2013-02-07 17:41:50 -0500

    block: vhdx initial file create
    Initial file create, and structure definitions from the
    specification.
    
    Note, this is an interim commit, and will be squashed and cleaned
    up prior to upstream submission.

---
+++ block/vhdx.c
@@ -0,0 +1,396 @@
+/*
+ * Block driver for Hyper-V VHDX Images
+ *
+ * Copyright (c) 2013 Red Hat, Inc.,
+ *
+ * Authors:
+ *  Jeff Cody <jcody@redhat.com>
+ *
+ *  This is based on the "VHDX Format Specification v0.95", published 4/12/2012
+ *  by Microsoft:
+ *      https://www.microsoft.com/en-us/download/details.aspx?id=29681
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ */
+
+#include "qemu-common.h"
+#include "block/block_int.h"
+#include "qemu/module.h"
+#include "migration/migration.h"
+#if defined(CONFIG_UUID)
+#include <uuid/uuid.h>
+#endif
+
+/* Structures and fields present in the VHDX file */
+
+
+/* ---- HEADER SECTION STRUCTURES ---- */
+
+typedef struct vhdx_file_identifier {
+    uint64_t    signature;      /* "vhdxfile" */
+    uint16_t    creator[256];   /* optional; utf-16 string to identify
+                                   the vhdx file creator.  Diagnotistic only */
+} vhdx_file_identifier;
+
+
+typedef struct vhdx_header {
+    uint32_t    signature;              /* "head" in ASCII */
+    uint32_t    checksum;               /* CRC-32C hash of the whole header */
+    uint64_t    sequence_name;          /* Seq number of this header.  Each
+                                           VHDX file has 2 of these headers,
+                                           and only the header with the highest
+                                           sequence number is valid */
+    uint8_t     file_write_guid[16];    /* 128 bit unique identifier. Must be
+                                           updated to new, unique value before
+                                           the first modification is made to 
+                                           file */
+    uint8_t     data_write_guid[16];    /* 128 bit unique identifier. Must be
+                                           updated to new, unique value before
+                                           the first modification is made to 
+                                           visible data.   Visbile data is
+                                           defined as:
+                                                    - system & user metadata
+                                                    - raw block data
+                                                    - disk size
+                                                    - any change that will
+                                                      cause the virtual disk
+                                                      sector read to differ 
+                                           
+                                           This does not need to change if
+                                           blocks are re-arranged */
+    uint8_t     log_guid[16];           /* 128 bit unique identifier. If zero,
+                                           there is no valid log. If non-zero,
+                                           log entries with this guid are
+                                           valid. */
+    uint16_t    log_version;            /* version of the log format.  Must not be
+                                           zero, unless log_guid is also zero */
+    uint16_t    version;                /* version of th evhdx file.  Currently,
+                                           only supported version is "1" */
+    uint32_t    log_length;             /* length of the log.  Must be a multiple
+                                           of 1024*1024 bytes */
+    uint32_t    log_offset              /* byte offset in the file of the log.  Must
+                                           also be a multiple of 1024*1024 bytes */
+    uint8_t     reserved[502];
+} vhdx_header;
+
+
+/* Header for the region table block */
+typedef struct vhdx_region_table_header {
+    uint32_t    signature;      /* "regi" in ASCII */
+    uint32_t    checksum;       /* CRC-32C hash of the 64KB table */
+    uint32_t    entry_count;    /* number of valid entries */
+    uint32_t    reserved;
+} vhdx_region_table_header;
+
+/* Individual region table entry.  There may be a maximum of 2047 of these
+ *
+ *  There are two known region table properties.  Both are required.
+ *  BAT (block allocation table):  2DC27766F62342009D64115E9BFD4A08
+ *  Metadata:                      8B7CA20647904B9AB8FE575F050F886E
+ */  
+typedef struct vhdx_region_table_entry {
+    uint8_t     guid[16];       /* 128-bit unique identifier */
+    uint64_t    file_offset;    /* offset of the object in the file.  Must be
+                                   multiple of 1MB */
+    uint32_t    length;         /* length, in bytes, of the object */
+    uint32_t    required:1;     /* indicates if this region must be recognized
+                                   in order to load the file */
+    uint32_t    reserved:31;
+} vhdx_region_table_entry;
+
+
+
+
+
+
+
+
+
+/* ---- LOG ENTRY STRUCTURES ---- */
+
+typedef struct vhdx_log_entry_header {
+    uint32_t    signature;              /* "loge" in ASCII */
+    uint32_t    checksum;               /* CRC-32C hash of the 64KB table */
+    uint32_t    entry_length;           /* length in bytes, multiple of 1MB */
+    uint32_t    tail;                   /* byte offset of first log entry of a
+                                           seq, where this entry is the last
+                                           entry */
+    uint64_t    sequence_number;        /* incremented with each log entry.
+                                           May not be zero. */
+    uint32_t    descriptor_count;       /* number of descriptors in this log
+                                           entry, must be >= 0 */
+    uint32_t    reserved;   
+    uint8_t     log_guid[16];           /* value of the log_guid from
+                                           vhdx_header.  If not found in
+                                           vhdx_header, it is invalid */
+    uint64_t    flushed_file_offset;    /* see spec for full details - this
+                                           sould be vhdx file size in bytes */
+    uint64_t    last_file_offset;       /* size in bytes that all allocated
+                                           file structures fit into */
+} vhdx_log_entry_header;
+
+
+typedef struct vhdx_log_zero_descriptor {
+    uint32_t    zero_signature;         /* "zero" in ASCII */
+    uint32_t    reserver;
+    uint64_t    zero_length;            /* length of the section to zero */
+    uint64_t    file_offset;            /* file offset to write zeros - multiple
+                                           of 4kB */
+    uint64_t    sequence_number;        /* must match same field in 
+                                           vhdx_log_entry_header */
+} vhdx_log_zero_descriptor;
+
+
+typedef struct vhdx_log_data_descriptor {
+    uint32_t    data_signature;         /* "desc" in ASCII */
+    uint32_t    trailing_bytes;         /* bytes 4092-4096 of the data sector */
+    uint64_t    leading_bytes;          /* bytes 0-7 of the data sector */
+    uint64_t    file_offset;            /* file offset where the data described
+                                           herein is written */
+    uint64_t    sequence_number;        /* must match the sequence number field
+                                           in entry header */
+} vhdx_log_data_descriptor;
+
+
+typedef vhdx_log_data_sector {
+    uint32_t    data_signature;         /* "data" in ASCII */
+    uint32_t    sequence_high;          /* 4 MSB of 8 byte sequence_number */
+    uint8_t     data[4084];             /* raw data, bytes 8-4091 (inclusive).
+                                           see the data descriptor field for the
+                                           other mising bytes */
+    uint32_t    sequence_low;           /* 4 LSB of 8 byte sequence_number */
+} vhdx_log_data_sector;
+
+
+
+
+
+
+
+/* ---- METADATA REGION STRUCTURES ---- */
+
+typedef struct vhdx_metadata_table_header {
+    uint64_t    signature;      /* "metadata" in ASCII */
+    uint16_t    reserved;
+    uint16_t    entry_count;    /* number of entries in table.  <= 2047 */
+    uint32_t    reserved2[5];
+} vhdx_metadata_table_header;
+
+typedef struct vhdx_metadata_table_entry {
+    uint8_t     item_id[16];        /* 128-bit identifier for metadata */
+    uint32_t    offset;             /* byte offset of the metadata.  At least
+                                       64kB.  Relative to start of metadata
+                                       region */
+                                    /* note: if length is zero, so is offset */
+    uint32_t    length;             /* length in bytes of metadata. <= 1MB. */ 
+    uint32_t    is_user:1;          /* 1: user metadata, 0: system metadata
+                                       max of 1024 entries can have this set */
+    uint32_t    is_virtual_disk:1;  /* See spec.  1: virtual disk metadata
+                                                  0: file metadata */
+    uint32_t    is_required:1;      /* 1: parser must understand this data */
+    uint32_t    reserved:29;
+    uint32_t    reserved2;
+}
+
+typedef struct vhdx_virtual_disk_size {
+    uint64_t    virtual_disk_size;  /* Size of the virtual disk, in bytes.
+                                       Must be a multiple of the sector size,
+                                       max of 64TB */
+} vhdx_virtual_disk_size;
+
+typedef struct vhdx_page83_data {
+    uint8_t     page_83_data[16];   /* unique id for scsi devices that support
+                                       page 0x83 */
+} vhdx_page83_data;
+
+typedef struct vhdx_virtual_disk_logical_sector_size {
+    uint32_t    logical_sector_size;    /* virtual disk sector size (in bytes).
+                                           Can only be 512 or 4096 bytes */
+} vhdx_virtual_disk_logical_sector_size;
+
+typedef struct vhdx_virtual_disk_physical_sector_size {
+    uint32_t    physical_sector_size;   /* physical sector size (in bytes).
+                                           Can only be 512 or 4096 bytes */
+} vhdx_virtual_disk_physical_sector_size;
+
+typedef struct vhdx_parent_locator_header {
+    uint8_t     locator_type[16];  /* type of the parent virtual disk. */
+    uint16_t    reserved;
+    uint16_t    key_value_count;   /* number of key/value pairs for this
+                                      locator */
+} vhdx_parent_locator_header;
+
+/* key and value strings are UNICODE strings, UTF-16 LE encoding, no NULs */
+typedef struct vhdx_parent_locator_entry {
+    uint32_t    key_offset;         /* offset within metadata for key, > 0 */
+    uint32_t    value_offset;       /* offset within metadata for value, >0 */
+    uint16_t    key_length;         /* length of entry key, > 0 */
+    uint16_t    value_length;       /* length of entry value, > 0 */
+} vhdx_parent_locator_entry;
+
+
+/* ----- END VHDX SPECIFICATION STRUCTURES ---- */
+
+
+
+
+typedef struct BDRVVHDXState {
+    CoMutex lock;
+
+    /* TODO */
+
+} BDRVVHDXState;
+
+/* CRC-32C, Castagnoli polynomial, code 0x11EDC6F41 */
+static uint32_t vhdx_checksum(uint8_t* buf, size_t size)
+{
+
+    /* TODO */
+
+    return 0;
+}
+
+/* 
+ * Per the MS VHDX Specification, for every VHDX file: 
+ *      - The header section is fixed size - 1 MB
+ *      - The header section is always the first "object"
+ *      - The first 64KB of the header is the File Identifier
+ *      - The first uint64 (8 bytes) is the VHDX Signature ("vhdxfile")
+ *      - The following 512 bytes constitute a UTF-16 string identifiying the
+ *        software that created the file, and is optional and diagnostic only.
+ *
+ *  Therefore, we probe by looking for the vhdxfile signature "vhdxfile"
+ */
+static int vhdx_probe(const uint8_t *buf, int buf_size, const char *filename)
+{
+    if (buf_size >= 8 && !strncmp((char *)buf, "conectix", 8))
+	return 100;
+    return 0;
+}
+
+static int vhdx_open(BlockDriverState *bs, int flags)
+{
+    BDRVVHDXState *s = bs->opaque;
+    int ret;
+
+
+    /* TODO */
+
+    return ret;
+}
+
+static int vhdx_reopen_prepare(BDRVReopenState *state,
+                               BlockReopenQueue *queue, Error **errp)
+{
+    return 0;
+}
+
+
+
+static int vhdx_read(BlockDriverState *bs, int64_t sector_num,
+                    uint8_t *buf, int nb_sectors)
+{
+    BDRVVHDXState *s = bs->opaque;
+    int ret;
+
+    /* TODO */
+
+    return 0;
+}
+
+static coroutine_fn int vhdx_co_read(BlockDriverState *bs, int64_t sector_num,
+                                    uint8_t *buf, int nb_sectors)
+{
+    int ret;
+    BDRVVHDXState *s = bs->opaque;
+    qemu_co_mutex_lock(&s->lock);
+    ret = vhdx_read(bs, sector_num, buf, nb_sectors);
+    qemu_co_mutex_unlock(&s->lock);
+
+    /* TODO */
+
+    return ret;
+}
+
+static int vhdx_write(BlockDriverState *bs, int64_t sector_num,
+    const uint8_t *buf, int nb_sectors)
+{
+    BDRVVHDXState *s = bs->opaque;
+
+    /* TODO */
+
+    return 0;
+}
+
+static coroutine_fn int vhdx_co_write(BlockDriverState *bs, int64_t sector_num,
+                                     const uint8_t *buf, int nb_sectors)
+{
+    int ret;
+    BDRVVHDXState *s = bs->opaque;
+    qemu_co_mutex_lock(&s->lock);
+    ret = vhdx_write(bs, sector_num, buf, nb_sectors);
+    qemu_co_mutex_unlock(&s->lock);
+
+    /* TODO */
+
+    return ret;
+}
+
+
+static int vhdx_create(const char *filename, QEMUOptionParameter *options)
+{
+
+    /* TODO */
+
+   return 0;
+}
+
+static void vhdx_close(BlockDriverState *bs)
+{
+
+    /* TODO */
+
+    BDRVVHDXState *s = bs->opaque;
+}
+
+static QEMUOptionParameter vhdx_create_options[] = {
+    {
+        .name = BLOCK_OPT_SIZE,
+        .type = OPT_SIZE,
+        .help = "Virtual disk size"
+    },
+    {
+        .name = BLOCK_OPT_SUBFMT,
+        .type = OPT_STRING,
+        .help =
+            ""
+    },
+    { NULL }
+};
+
+static BlockDriver bdrv_vhdx = {
+    .format_name    = "vhdx",
+    .instance_size  = sizeof(BDRVVHDXState),
+
+    .bdrv_probe     = vhdx_probe,
+    .bdrv_open      = vhdx_open,
+    .bdrv_close     = vhdx_close,
+    .bdrv_reopen_prepare = vhdx_reopen_prepare,
+    .bdrv_create    = vhdx_create,
+
+    .bdrv_read              = vhdx_co_read,
+    .bdrv_write             = vhdx_co_write,
+
+    .create_options = vhdx_create_options,
+};
+
+static void bdrv_vhdx_init(void)
+{
+    bdrv_register(&bdrv_vhdx);
+}
+
+block_init(bdrv_vhdx_init);
