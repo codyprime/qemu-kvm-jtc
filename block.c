bdcc8a64338f06bb1517bf0c07fc26bf5dafad46
Jeff Cody <jcody@redhat.com>
2014-01-17 12:17:33 -0500

    block: remove unnecessary code; image always r/w if snapshot=on
    Important note: this patch should only be applied after patch
     "block: do not allow read-only=on and snapshot=on to be used together"
    
    Now that 'read-only=on' and 'snapshot=on' are not allowed together,
    forcing open_flags to have BDRV_O_RDWR set is redundant code, and can be
    removed.  This is because unless read-only=on is specified, the image
    will always be opened with BDRV_O_RDWR set.

--- block.c
+++ block.c
@@ -722,13 +722,6 @@ static int bdrv_open_flags(BlockDriverState *bs, int flags)
      */
     open_flags &= ~(BDRV_O_SNAPSHOT | BDRV_O_NO_BACKING);
 
-    /*
-     * Snapshots should be writable.
-     */
-    if (bs->is_temporary) {
-        open_flags |= BDRV_O_RDWR;
-    }
-
     return open_flags;
 }
 
