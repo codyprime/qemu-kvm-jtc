fe48d23ac80ab122794ed4d087c4068e4a549a43
Jeff Cody <jcody@redhat.com>
2014-05-14 17:15:46 -0400

    block: extend block-commit to accept a string for the backing file
    On some image chains, QEMU may not always be able to resolve the
    filenames properly, when updating the backing file of an image
    after a block commit.
    
    For instance, certain relative pathnames may fail, or drives may
    have been specified originally by file descriptor (e.g. /dev/fd/???),
    or a relative protocol pathname may have been used.
    
    In these instances, QEMU may lack the information to be able to make
    the correct choice, but the user or management layer most likely does
    have that knowledge.
    
    With this extension to the block-commit api, the user is able to change
    the backing file of the overlay image as part of the block-commit
    operation.
    
    This allows the change to be 'safe', in the sense that if the attempt
    to write the overlay image metadata fails, then the block-commit
    operation returns failure, without disrupting the guest.
    
    If the commit top is the active layer, then specifying the backing
    file string will be treated as an error (there is no overlay image
    to modify in that case).
    
    If a backing file string is not specified in the command, the backing
    file string to use is determined in the same manner as it was
    previously.

--- blockdev.c
+++ blockdev.c
@@ -1916,6 +1916,7 @@ void qmp_block_commit(bool has_device, const char *device,
                       bool has_base_node_name, const char *base_node_name,
                       bool has_top, const char *top,
                       bool has_top_node_name, const char *top_node_name,
+                      bool has_backing_file, const char *backing_file,
                       bool has_speed, int64_t speed,
                       Error **errp)
 {
@@ -2031,11 +2032,16 @@ void qmp_block_commit(bool has_device, const char *device,
     }
 
     if (top_bs == bs) {
+        if (has_backing_file) {
+            error_setg(errp, "'backing-file' specified,"
+                             " but 'top' is the active layer");
+            return;
+        }
         commit_active_start(bs, base_bs, speed, on_error, block_job_cb,
                             bs, &local_err);
     } else {
         commit_start(bs, base_bs, top_bs, speed, on_error, block_job_cb, bs,
-                    &local_err);
+                     has_backing_file ? backing_file : NULL, &local_err);
     }
     if (local_err != NULL) {
         error_propagate(errp, local_err);
